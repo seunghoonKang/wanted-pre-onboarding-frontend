9e6bb29e65feaadb843d4540080acd963cf7a513
seunghoon <tmdgns4321@gmail.com>
2023-02-07 12:34:43 +0900

    라우팅 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import Register from "./components/Register";
+import { Route, Routes } from "react-router-dom";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import Main from "./pages/Main";
+import Todo from "./pages/Todo";
 
 function App() {
   return (
     <div>
-      <Register />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
     </div>
   );
 }
